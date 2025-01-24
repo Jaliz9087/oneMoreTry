781e1fe4f27af84e1c41c3be9860c6ea7392d8c6
whey <jimmy.hole@mail.ru>
2025-01-25 01:22:07 +0300

    pls

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -4,10 +4,11 @@ import org.junit.jupiter.api.BeforeAll;
 public class TestBase {
     @BeforeAll
     static void beforeAll() {
+        String remoteHost = System.getProperty("remoteHost", "localhost");
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.remote = "https://user1:1234@" + remoteHost + "/wd/hub";
 
 
 
