e4a44ae80d4c399e1dda1ea1f2b4feaab1d9adf1
whey <jimmy.hole@mail.ru>
2025-01-25 01:23:48 +0300

    pls1

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -6,6 +6,7 @@ public class TestBase {
     static void beforeAll() {
         String remoteHost = System.getProperty("remoteHost", "localhost");
         Configuration.browserSize = "1920x1080";
+        Configuration.timeout = 10000;
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
         Configuration.remote = "https://user1:1234@" + remoteHost + "/wd/hub";
