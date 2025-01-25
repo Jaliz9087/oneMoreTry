648665c081a2ae97084713c2e5f8bbab8e9603c6
whey <jimmy.hole@mail.ru>
2025-01-25 17:50:06 +0300

    come on

--- src/test/java/pges/components/CalendarComponent.java
+++ src/test/java/pges/components/CalendarComponent.java
@@ -6,9 +6,8 @@ import static com.codeborne.selenide.Selenide.$$;
 
 public class CalendarComponent {
     public void SetDate(String day, String month, String year) {
-
-        $(".react-datepicker__month-select").selectOptionContainingText(month);
         $(".react-datepicker__year-select").selectOptionByValue(year);
+        $(".react-datepicker__month-select").selectOptionContainingText(month);
         $$(".react-datepicker__day:not(.react-datepicker__day--outside-month)").findBy(text(day)).click();
     }
 }
